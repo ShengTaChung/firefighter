16952398e4e2888c24146895cc34dea1e2a10186
ShengTaChung <schung21@masonlive.gmu.edu>
2021-04-04 15:18:41 -0400

    init commit

---
+++ src/main/java/firefighters/FireDispatchImpl.java
@@ -0,0 +1,36 @@
+package firefighters;
+
+
+import api.City;
+import api.CityNode;
+import api.FireDispatch;
+import api.Firefighter;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+import java.util.List;
+
+public class FireDispatchImpl implements FireDispatch {
+
+  public FireDispatchImpl(City city) {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public void setFirefighters(int numFirefighters) {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public List<Firefighter> getFirefighters() {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public void dispatchFirefighers(CityNode... burningBuildings) {
+    // TODO
+    throw new NotImplementedException();
+  }
+}
---
+++ src/main/java/firefighters/FirefighterImpl.java
@@ -0,0 +1,20 @@
+package firefighters;
+
+import api.CityNode;
+import api.Firefighter;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+public class FirefighterImpl implements Firefighter {
+
+  @Override
+  public CityNode getLocation() {
+    // TODO
+    throw new NotImplementedException();
+  }
+
+  @Override
+  public int distanceTraveled() {
+    // TODO
+    throw new NotImplementedException();
+  }
+}
